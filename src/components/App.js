cd8be2630693a793c29d06dd8273f5f76046ce68
ebruersoy <ebruersoy.89@gmail.com>
2020-12-09 15:56:46 +0300

    deleteAppointments event

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import AddAppointments from "./AddAppointments";
 import SearchAppointments from "./SearchAppointments";
 import ListAppointments from "./ListAppointments";
 
+import { without } from "lodash";
+
 class App extends Component {
   constructor() {
     super();
@@ -12,6 +14,13 @@ class App extends Component {
       myAppointments: [],
       lastIndex: 0,
     };
+    this.deleteAppointments = this.deleteAppointments.bind(this);
+  }
+
+  deleteAppointments(apt) {
+    let tempApts = this.state.myAppointments;
+    tempApts = without(tempApts, apt);
+    this.setState({ myAppointments: tempApts });
   }
 
   componentDidMount() {
@@ -38,7 +47,10 @@ class App extends Component {
               <div className="container">
                 <AddAppointments />
                 <SearchAppointments />
-                <ListAppointments appointments={this.state.myAppointments} />
+                <ListAppointments
+                  appointments={this.state.myAppointments}
+                  deleteAppointments={this.deleteAppointments}
+                />
               </div>
             </div>
           </div>
